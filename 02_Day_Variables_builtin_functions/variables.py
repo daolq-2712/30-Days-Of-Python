13af8a8b5ba8d4ac703ace165c0da12c6c4d867b
le.quang.dao <le.quang.dao@sun-asterisk.com>
2025-07-29 06:45:37 +0700

    feat: day 2 - Variables & Math basic

--- 02_Day_Variables_builtin_functions/variables.py
+++ 02_Day_Variables_builtin_functions/variables.py
@@ -1,21 +1,26 @@
 
-# Variables in Python
+# Day 2: 30 Days of python programming
 
-first_name = 'Asabeneh'
-last_name = 'Yetayeh'
-country = 'Finland'
-city = 'Helsinki'
-age = 250
+# Exercises: Level 1 -  Variables in Python
+first_name = 'Le'
+last_name = 'Dao'
+full_name = first_name + ' ' + last_name
+country = 'Vietnam'
+city = 'Da Nang'
+age = 34
+years = 2025
 is_married = True
-skills = ['HTML', 'CSS', 'JS', 'React', 'Python']
+is_light_on = False
+height, weight = 1.73, 77  # in meters and kg
+skills = [ 'C++', 'Java', 'Kotlin', 'Dart', 'Go', 'Android', 'iOS', 'Flutter', 'Python', 'HTML', 'CSS', 'JavaScript', 'SQL', ]
 person_info = {
-    'firstname':'Asabeneh', 
-    'lastname':'Yetayeh', 
-    'country':'Finland',
-    'city':'Helsinki'
+    'firstname':'Le', 
+    'lastname':'Dao', 
+    'country':'Vietnam',
+    'city':'Da Nang'
     }
 
-# Printing the values stored in the variables
+# Exercises: Level 2 - Printing the values stored in the variables
 
 print('First name:', first_name)
 print('First name length:', len(first_name))
@@ -28,10 +33,23 @@ print('Married: ', is_married)
 print('Skills: ', skills)
 print('Person information: ', person_info)
 
+print('Compare first name and last name length:', len(first_name) == len(last_name))
+num_one,num_two = 5, 4
+total = num_one + num_two
+print('Sum of num_one and num_two:', total)
+diff = num_one - num_two
+print('Difference between num_one and num_two:', diff)
+product = num_one * num_two
+print('Product of num_one and num_two:', product)
+division = num_one / num_two
+print('Division of num_one by num_two:', division)
+remainder = num_one % num_two
+print('Remainder of num_one divided by num_two:', remainder)
+exponent = num_one ** num_two
+print('Exponent of num_one raised to num_two:', exponent)
+floor_division = num_one // num_two
+
 # Declaring multiple variables in one line
-
-first_name, last_name, country, age, is_married = 'Asabeneh', 'Yetayeh', 'Helsink', 250, True
-
 print(first_name, last_name, country, age, is_married)
 print('First name:', first_name)
 print('Last name: ', last_name)
