28b816ca813051c1abebe4ef20f649302f97501a
le.quang.dao <le.quang.dao@sun-asterisk.com>
2025-07-28 16:02:00 +0700

    feat: day one - install python & data types

--- 01_Day_Introduction/helloworld.py
+++ 01_Day_Introduction/helloworld.py
@@ -1,23 +1,30 @@
 # Introduction
 # Day 1 - 30DaysOfPython Challenge
 
-print(3 + 2)   # addition(+)
-print(3 - 2)   # subtraction(-)
-print(3 * 2)   # multiplication(*)
-print(3 / 2)   # division(/)
-print(3 ** 2)  # exponential(**)
-print(3 % 2)   # modulus(%)
-print(3 // 2)  # Floor division operator(//)
+print(3 + 4)   # addition(+)
+print(3 - 4)   # subtraction(-)
+print(3 * 4)   # multiplication(*)
+print(3 / 4)   # division(/)
+print(3 ** 4)  # exponential(**)
+print(3 % 4)   # modulus(%)
+print(3 // 4)  # Floor division operator(//)
 
 # Checking data types
-
 print(type(10))                  # Int
 print(type(3.14))                # Float
 print(type(1 + 3j))              # Complex
 print(type('Asabeneh'))          # String
+print(type(True))                # Bool
 print(type([1, 2, 3]))           # List
 print(type({'name':'Asabeneh'})) # Dictionary
 print(type({9.8, 3.14, 2.7}))    # Set
 print(type((9.8, 3.14, 2.7)))    # Tuple
 print(type(3 == 3))              # Bool
 print(type(3 >= 3))              # Bool
+
+# Euclidian distance between (2, 3) and (10, 8)
+import math
+x1, y1 = 2, 3
+x2, y2 = 10, 8
+distance = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+print(f"Euclidian distance: {distance}")
